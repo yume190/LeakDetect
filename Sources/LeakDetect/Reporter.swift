0a570fa97bd78bea316c73b6138437b44fc537b7
Yume <yume190@gmail.com>
2023-09-13 13:23:44 +0800

    fix reporter report content

--- Sources/LeakDetect/Reporter.swift
+++ Sources/LeakDetect/Reporter.swift
@@ -1,49 +1,49 @@
 //
-//  File.swift
-//  
+//  Reporter.swift
+//
 //
 //  Created by Yume on 2023/9/7.
 //
 
+import ArgumentParser
 import Foundation
 import LeakDetectKit
-import ArgumentParser
 
 public enum Reporter {
-    public typealias R = (LeakResult) -> Void
+  public typealias R = (LeakResult) -> Void
     
-    case xcode
-    case vscode
-    case custom(R)
+  case xcode
+  case vscode
+  case custom(R)
     
-    public func report(_ result: LeakResult) {
-        switch self {
-        case .vscode:
-            print("\(result.location.reportVSCode) \(result.reason)")
-        case .xcode:
-            print("\(result.location.reportXCode) \(result.reason)")
-        case let .custom(report):
-            report(result)
-        }
+  public func report(_ result: LeakResult) {
+    switch self {
+    case .vscode:
+      print("\(result.location.reportVSCode) \(result.reportReason)")
+    case .xcode:
+      print("\(result.location.reportXCode) \(result.reportReason)")
+    case let .custom(report):
+      report(result)
     }
+  }
 }
 
 extension Reporter: ExpressibleByArgument {
-    public init?(argument: String) {
-        
-        switch argument {
-        case "xcode":
-            self = .xcode
-        case "vscode":
-            self = .vscode
-        default:
-            self = .custom { _ in }
-        }
+  public init?(argument: String) {
+    switch argument {
+    case "xcode":
+      self = .xcode
+    case "vscode":
+      self = .vscode
+    default:
+      self = .custom { _ in }
     }
-    public var defaultValueDescription: String { "vscode" }
+  }
+
+  public var defaultValueDescription: String { "vscode" }
     
-    public static var allValueStrings: [String] { ["xcode", "vscode", "custom"] }
-    public static var defaultCompletionKind: CompletionKind { .list(allValueStrings) }
+  public static var allValueStrings: [String] { ["xcode", "vscode", "custom"] }
+  public static var defaultCompletionKind: CompletionKind { .list(allValueStrings) }
     
-    static let all: String = "xcode|vscode|custom"
+  static let all: String = "xcode|vscode|custom"
 }
